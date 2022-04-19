ed903ebbde8e1c8655afb5705b6e184f81e7a81a
Roman Latypov <jiggawhat5@gmail.com>
2022-04-19 18:01:25 +0500

    added markup sing-in and sign-out

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <section className="sign">
+      <form className="sign__form">
+        <h2 className="sign__title">Вход</h2>
+        <input
+          className="sign__input"
+          type="email"
+          placeholder="Email"
+          name=""
+          id=""/>
+        <span></span>
+        <input
+          className="sign__input"
+          type="password"
+          placeholder="Пароль"
+          name=""
+          id=""/>
+        <span></span>
+        <button className="sign__button">Войти</button>
+      </form>
+    </section>
   );
 };
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-
-    </div>
+      <section className="sign">
+      <form className="sign__form">
+        <h2 className="sign__title">Регистрация</h2>
+        <input
+          className="sign__input"
+          type="email"
+          placeholder="Email"
+          name=""
+          id=""/>
+        <span></span>
+        <input
+          className="sign__input"
+          type="password"
+          placeholder="Пароль"
+          name=""
+          id=""/>
+        <span></span>
+        <button className="sign__button">Зарегистрироваться</button>
+        <p className="sign__redirect-text">Уже зарегистрированы?
+          <Link to="Login.js" className="sign__redirect-link">Войти</Link>
+        </p>
+      </form>
+    </section>
   );
 };
 
